31f0d4370468e8a191d16dcc66f028b551254b5a
ArugaYudish <ayudish7@gmail.com>
2026-07-11 13:36:46 +0700

    Update gold_performance_fdos.sql

--- models/gold_performance_fdos.sql
+++ models/gold_performance_fdos.sql
@@ -16,19 +16,19 @@ WITH current_operational AS (
     LIMIT 1
 ),
 
--- Tarik data mentah langsung cross join dengan m_cycle3 untuk hitung is_ytd
+-- FIX: Arahkan source ke silver_sales_performance_parent yang punya kolom stock lengkap
 prep_data AS (
     SELECT 
-        fdos.*,
+        parent.*,
         c.cur_year AS op_current_year,
         c.cur_period AS op_current_period,
         c.cur_week AS op_current_week,
-        CASE WHEN fdos.week::numeric <= c.cur_week THEN 1 ELSE 0 END AS is_ytd
-    FROM spx.silver_performance_fdos fdos
+        CASE WHEN parent.week::numeric <= c.cur_week THEN 1 ELSE 0 END AS is_ytd
+    FROM spx.silver_sales_performance_parent parent
     CROSS JOIN current_operational c
 ),
 
--- Proses Unpivot Vertikal langsung dari prep_data
+-- Proses Unpivot Vertikal metrik QTY dan VALUE beserta pasangan komponen SCD-nya
 unpivoted_metrics AS (
     -- Blok QTY
     SELECT 
@@ -39,7 +39,10 @@ unpivoted_metrics AS (
         
         'QTY' AS pilihan_satuan,
         sta_qty AS sta_value_final,
-        fdos_update AS fdos_value_final
+        fdos_update AS fdos_value_final,
+        -- Komponen SCD untuk QTY dari table parent
+        stock_qty AS stock_subdist_final,
+        avg_5w_qty AS avg_stm_5w_final
     FROM prep_data
 
     UNION ALL
@@ -53,8 +56,72 @@ unpivoted_metrics AS (
         
         'VALUE' AS pilihan_satuan,
         sta_value AS sta_value_final,
-        fdos_value AS fdos_value_final
+        fdos_value AS fdos_value_final,
+        -- Komponen SCD untuk VALUE dari table parent
+        stock_value AS stock_subdist_final,
+        avg_5w_value AS avg_stm_5w_final
     FROM prep_data
 )
 
 SELECT * FROM unpivoted_metrics
+
+-- {{ config(
+--     materialized='table',
+--     alias='gold_performance_fdos',
+--     indexes=[
+--       {'columns': ['year', 'week', 'pilihan_satuan']}
+--     ]
+-- ) }}
+
+-- WITH current_operational AS (
+--     SELECT 
+--         year::text AS cur_year,
+--         period::text AS cur_period,
+--         week::numeric AS cur_week
+--     FROM spx.m_cycle3 
+--     WHERE cdate::date = CURRENT_DATE
+--     LIMIT 1
+-- ),
+
+-- -- Tarik data mentah langsung cross join dengan m_cycle3 untuk hitung is_ytd
+-- prep_data AS (
+--     SELECT 
+--         fdos.*,
+--         c.cur_year AS op_current_year,
+--         c.cur_period AS op_current_period,
+--         c.cur_week AS op_current_week,
+--         CASE WHEN fdos.week::numeric <= c.cur_week THEN 1 ELSE 0 END AS is_ytd
+--     FROM spx.silver_performance_fdos fdos
+--     CROSS JOIN current_operational c
+-- ),
+
+-- -- Proses Unpivot Vertikal langsung dari prep_data
+-- unpivoted_metrics AS (
+--     -- Blok QTY
+--     SELECT 
+--         channel, year, period, week, flag, distributor_id, parent_id,
+--         nsm_id, nsm_name, grsm_id, grsm_name, rsm_id, rsm_name, ss_id, ss_name,
+--         op_current_year, op_current_period, op_current_week, is_ytd,
+--         CURRENT_TIMESTAMP AS loaded_at,
+        
+--         'QTY' AS pilihan_satuan,
+--         sta_qty AS sta_value_final,
+--         fdos_update AS fdos_value_final
+--     FROM prep_data
+
+--     UNION ALL
+
+--     -- Blok VALUE
+--     SELECT 
+--         channel, year, period, week, flag, distributor_id, parent_id,
+--         nsm_id, nsm_name, grsm_id, grsm_name, rsm_id, rsm_name, ss_id, ss_name,
+--         op_current_year, op_current_period, op_current_week, is_ytd,
+--         CURRENT_TIMESTAMP AS loaded_at,
+        
+--         'VALUE' AS pilihan_satuan,
+--         sta_value AS sta_value_final,
+--         fdos_value AS fdos_value_final
+--     FROM prep_data
+-- )
+
+-- SELECT * FROM unpivoted_metrics
