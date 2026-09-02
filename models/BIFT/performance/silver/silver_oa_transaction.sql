d76bb4cc37adaeef4a378ce47ddd4551ef14e54c
Muhammad Fadli Hasan <winnerfadli999@gmail.com>
2026-09-02 22:44:17 +0700

    feat: update table alias and refactor sales hierarchy join logic into transaction CTE

--- models/BIFT/performance/silver/silver_oa_transaction.sql
+++ models/BIFT/performance/silver/silver_oa_transaction.sql
@@ -2,7 +2,7 @@
     config(
         schema='bift',
         materialized='table',
-        alias='silver_oa_transaction',
+        alias='silver_oa_transaction_dev',
         unique_key=['distributor_id', 'sls_id', 'cust_id', 'tahun', 'periode', 'inv_no', 'pcode'],
         indexes=[
           {'columns': ['tahun', 'periode', 'distributor_id', 'sls_id', 'cust_id'], 'type': 'btree'},
@@ -51,60 +51,91 @@ WITH bronze_cb_distinct AS (
 vd AS (
     {% for w in range(18, 19) %}
     SELECT 
-        subdist_id,
-        custno,
-        slsno,
-        slsfc_id,
-        prd_no AS periode,
-        "year" AS tahun,
-        week_no,
-        ord_date,
-        inv_date,
-        inv_no,
-        pcode,
-        inv_qty,
-        inv_val
-    FROM spx.vfsales_det
-    WHERE week_no = {{ w }}
-      AND sts = '905'
-      AND inv_val > 0
+        v.subdist_id,
+        v.custno,
+        v.slsno,
+        v.slsfc_id,
+        v.prd_no AS periode,
+        v."year" AS tahun,
+        v.week_no,
+        v.ord_date,
+        v.inv_date,
+        v.inv_no,
+        v.pcode,
+        v.inv_qty,
+        v.inv_val,
+
+        -- Sales Hierarchy details attached directly inside CTE
+        COALESCE(sh.source_schema, 'm1') AS source_schema,
+        sh.gdiv_id,
+        sh.gdiv_nm,
+        sh.sd_id,
+        sh.sd_nm,
+        sh.nsm_id,
+        sh.nsm_nm,
+        sh.grsm_id,
+        sh.grsm_nm,
+        sh.rsm_id,
+        sh.rsm_nm,
+        sh.ss_id,
+        sh.ss_nm,
+        sh.distributor_nm,
+        sh.sls_nm,
+        COALESCE(sh.salesforce_id, v.slsfc_id) AS salesforce_id,
+        sh.salesforce_nm,
+        sh.salesforce_div_id,
+        sh.salesforce_div_nm,
+        sh.team_id,
+        sh.opr_type
+    FROM spx.vfsales_det v
+    INNER JOIN bift.dim_salesman_hierarchy sh 
+        ON v.subdist_id = sh.distributor_id
+       AND v.slsno      = sh.sls_id
+       AND (
+            sh.termin_year IS NULL
+            OR v."year" < sh.termin_year
+            OR (v."year" = sh.termin_year AND v.prd_no <= sh.termin_period)
+       )
+    WHERE v.week_no = {{ w }}
+      AND v.sts = '905'
+      AND v.inv_val > 0
     {% if not loop.last %} UNION ALL {% endif %}
     {% endfor %}
 )
 
 SELECT 
-    COALESCE(sh.source_schema, cb.source_schema) AS source_schema,
+    vd.source_schema,
     cb.tahun,
     cb.periode,
     vd.week_no::numeric AS week,
     vd.ord_date::date AS date,
     vd.inv_date::date AS inv_date,
-    sh.gdiv_id,
-    sh.gdiv_nm,
-    sh.sd_id,
-    sh.sd_nm,
-    sh.nsm_id,
-    sh.nsm_nm,
-    sh.grsm_id,
-    sh.grsm_nm,
-    sh.rsm_id,
-    sh.rsm_nm,
-    sh.ss_id,
-    sh.ss_nm,
+    vd.gdiv_id,
+    vd.gdiv_nm,
+    vd.sd_id,
+    vd.sd_nm,
+    vd.nsm_id,
+    vd.nsm_nm,
+    vd.grsm_id,
+    vd.grsm_nm,
+    vd.rsm_id,
+    vd.rsm_nm,
+    vd.ss_id,
+    vd.ss_nm,
     cb.distributor_id,
-    COALESCE(sh.distributor_nm, cb.distributor_nm) AS distributor_nm,
+    COALESCE(vd.distributor_nm, cb.distributor_nm) AS distributor_nm,
     vd.slsno AS sls_id,
-    sh.sls_nm,
+    vd.sls_nm,
     mmgs.gsalesforce1_id,
     mmgs.gsalesforce1_nm,
     mmgs.gsalesforce2_id,
     mmgs.gsalesforce2_nm,
-    COALESCE(sh.salesforce_id, vd.slsfc_id) AS salesforce_id,
-    COALESCE(mmgs.salesforce_nm, sh.salesforce_nm) AS salesforce_nm,
-    sh.salesforce_div_id,
-    sh.salesforce_div_nm,
-    sh.team_id,
-    sh.opr_type,
+    vd.salesforce_id,
+    COALESCE(mmgs.salesforce_nm, vd.salesforce_nm) AS salesforce_nm,
+    vd.salesforce_div_id,
+    vd.salesforce_div_nm,
+    vd.team_id,
+    vd.opr_type,
     cb.cust_id,
     cb.cust_nm,
     cb.channel_id,
@@ -153,16 +184,8 @@ INNER JOIN bronze_cb_distinct cb
    AND vd.custno     = cb.cust_id
    AND vd.periode    = cb.periode
    AND vd.tahun      = cb.tahun
-INNER JOIN bift.dim_salesman_hierarchy sh 
-    ON vd.subdist_id = sh.distributor_id
-   AND vd.slsno      = sh.sls_id
-   AND (
-        sh.termin_year IS NULL
-        OR cb.tahun < sh.termin_year
-        OR (cb.tahun = sh.termin_year AND cb.periode <= sh.termin_period)
-   )
 LEFT JOIN bift.dim_mapping_group_salesforce mmgs 
-    ON sh.salesforce_id = mmgs.salesforce_id
-   AND sh.source_schema = mmgs.source_schema
+    ON vd.salesforce_id = mmgs.salesforce_id
+   AND vd.source_schema = mmgs.source_schema
 LEFT JOIN bift.dim_product f 
     ON vd.pcode = f.pcode
