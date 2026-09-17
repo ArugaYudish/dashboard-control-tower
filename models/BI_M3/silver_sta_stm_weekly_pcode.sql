af5b5a4340b98b0062adae08f4f12637f4e72db7
ArugaYudish <76808196+ArugaYudish@users.noreply.github.com>
2026-09-17 15:42:01 +0700

    Update silver_sta_stm_weekly_pcode.sql

--- models/BI_M3/silver_sta_stm_weekly_pcode.sql
+++ models/BI_M3/silver_sta_stm_weekly_pcode.sql
@@ -13,26 +13,29 @@ with stm_summary as (
         pcode,
         sum(omsetqty) as stm_qty,
         sum(omsetvalue) as stm_val
-    from spx.v_omset_subdist_weekly_bw -- Ganti ref() jadi nama tabel langsung
+    from spx.v_omset_subdist_weekly_bw
     group by 1, 2, 3, 4
 ),
 
 sta_summary as (
     select 
-        cast(year as int) as year,
-        cast(week as int) as week,
+        cast(year as int) as year,    -- Mengambil kolom 'year' asli
+        cast(week as int) as week,    -- Mengambil kolom 'week' asli
         distributor_id,
         pcode,
         sum(sta_qty) as sta_qty,
         sum(sta_value) as sta_val
-    from spx.v_sta_subdist -- Ganti ref() jadi nama tabel langsung
+    from spx.v_sta_subdist
     group by 1, 2, 3, 4
 )
 
 select 
     coalesce(sta.year, stm.year) as year,
     coalesce(sta.week, stm.week) as week,
+    
+    -- FORMAT LENGKAP: Week (2 digit) . Year (4 digit) -> Hasil: 30.2025
     lpad(coalesce(sta.week, stm.week)::text, 2, '0') || '.' || coalesce(sta.year, stm.year)::text as week_year,
+    
     coalesce(sta.distributor_id, stm.distributor_id) as distributor_id,
     coalesce(sta.pcode, stm.pcode) as pcode,
     
